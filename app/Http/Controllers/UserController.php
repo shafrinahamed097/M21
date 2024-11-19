ff29f6250593f5e8483beb43cf792735c436d5ce
shafrinahamed97 <shafrinahamedjob@gmail.com>
2024-11-19 14:08:21 +0600

    Complete User Registration Method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,17 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    /**
-     * User registration API
-     * 
-     * @param Request $request
-     * @return void
-     */
+  
     function UserRegistration(Request $request){
+
+        try{User::create([
+            'firstName'=>$request->input('firstName'),
+            'lastName'=>$request->input('lastName'),
+            'email'=>$request->input('email'),
+            'mobile'=>$request->input('mobile'),
+            'password'=>$request->input('password'),
+        ]);
+        return response()->json([
+            'status' => "success",
+            'message'=>'User Registration Successfully'
+
+        ]);
+
+        }
+        catch(Exception $e)
+        {
+            return response()->json([
+                'status'=>'failed',
+                'message'=>'User Registration Failed'
+
+            ]);
+
+
+        }
+        
 
     }
 }
--- database/migrations/2024_11_19_061528_create_users.php
+++ database/migrations/2024_11_19_061528_create_users.php
@@ -13,12 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('firstName', 50);
+            $table->string('lastName', 50);
+            $table->string('email', 50)->unique();
+            $table->string('mobile');
+            $table->string('password', 50);
+            $table->string('otp', 10);
+            $table->string('created_at')->useCurrent();
+            $table->string('updated_at')->useCurrent()->useCurrentOnUpdate();
         });
     }
 
