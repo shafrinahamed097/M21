ed321bfc064d349056ad3139870b63503f4d6251
shafrinahamed97 <shafrinahamedjob@gmail.com>
2024-11-27 13:36:06 +0600

    Declare Category Update function

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -41,6 +41,15 @@ class CategoryController extends Controller
            return Category::where('id', $category_id)->where('user_id', $user_id)->delete();
        }
 
+       function CategoryUpdate(Request $request){
+        $category_id=$request->input('id');
+        $user_id=$request->header('id');
+        return Category::where('id', $category_id)->where('user_id', $user_id)
+        ->update([
+            'name'=>$request->input('name'),
+        ]);
+       }
+
 
 
 
